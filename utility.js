6d90a43a22dc2cf664c31a70967bdab92d16e654
Ankur Bansal <59500108+AnkurBansal18@users.noreply.github.com>
2021-08-01 13:02:09 +0530

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,13 @@
+let utility = {};
+utility.types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z','rar','tar','gz','ar','iso','xz'],
+    documents: ['docx','doc','odt','ods','odp','odg','odf','txt','ps','tex'],
+    excel: ['xlsx','xls', 'xlsb'],
+    pdfs: ['pdf'],
+    app : ['exe' , 'dmg', 'pkg', 'deb'],
+    presentation:['pptx'],
+    image :['jpeg','jpg','png','psd','gif']
+};
+
+module.exports = utility;
